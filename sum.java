588149e2de591d56abd13c2eb6af50fae3b15e16
Uzma Shaikh <uzmas1432@gmail.com>
2024-01-18 09:55:06 +0530

    Commited

--- sum.java
+++ sum.java
@@ -1,8 +1,8 @@
 public class Sum{
     public static void main(String[] args){
-    int a = 5;
-    int b = 6;
+    int a = 10;
+    int c = 5;
 
-    System.out.println(a+b);
+    System.out.println(a+c);
     }
 }
